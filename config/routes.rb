21874eb3543a4b3acaac8273e0e01190c4f0a452
Roy Chanunkha <roychanunkha@gmail.com>
2024-03-14 12:52:37 +0200

    adding locations to route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :reports, only: %i[show]
+      resources :locations, only: %i[index]
     end
   end
 end
