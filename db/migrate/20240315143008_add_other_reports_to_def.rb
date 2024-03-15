494df5c6d8bcf4cccfdb413cfbcbd68ffdb8284b
Roy Chanunkha <roychanunkha@gmail.com>
2024-03-15 16:42:57 +0200

    chore: add migration

---
+++ db/migrate/20240315143008_add_other_reports_to_def.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Adding reports
+class AddOtherReportsToDef < ActiveRecord::Migration[7.0]
+  def change
+    # this a hash key, value
+    # loop through
+    User.current = User.first
+    MalawiHivProgramReports::ReportMap::REPORTS.each do |key, value|
+      puts "processing #{key} report, found #{value}"
+      ReportType.find_or_create_by(name: key, creator: User.current.user_id, date_created: Time.now)
+    end
+  end
+end
